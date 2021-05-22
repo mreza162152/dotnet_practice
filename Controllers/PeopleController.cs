16187da9bed9627ce9774c93ec790306af5048c1
Mohammad <mreza162152@bscse.uiu.ac.bd>
2021-05-22 18:39:39 +0600

    project done

---
+++ Controllers/PeopleController.cs
@@ -0,0 +1,28 @@
+﻿using DemoApp_MVC5_.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DemoApp_MVC5_.Controllers
+{
+    public class PeopleController : Controller
+    {
+        // GET: People
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult ListPeople()
+        {
+            List<PersonModel> people = new List<PersonModel>();
+            people.Add(new PersonModel { FirstName = "Ayon", LastName = "Chowdhury", Age = 23, IsAlive = true });
+            people.Add(new PersonModel { FirstName = "abc", LastName = "def", Age = 50, IsAlive = true });
+            people.Add(new PersonModel { FirstName = "qwe", LastName = "ert", Age = 97, IsAlive = false   });
+
+            return View(people);
+        }
+
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DemoApp_MVC5_.MvcApplication" Language="C#" %>
